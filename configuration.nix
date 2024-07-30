55cfd450cad59196e7c278c378f271895ea6f432
StackDoubleFlow <ojaslandge@gmail.com>
2024-07-30 00:59:43 -0500

    Change timezone to Central

--- configuration.nix
+++ configuration.nix
@@ -31,7 +31,7 @@
   hardware.bluetooth.enable = true;
 
   # Set your time zone.
-  time.timeZone = "America/Los_Angeles";
+  time.timeZone = "America/Chicago";
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
